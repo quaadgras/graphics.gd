4f9b88a54aeccf3fd23de96de7a4d4ffed69770b
Quentin Quaadgras <quentin@splizard.com>
2025-09-09 09:03:46 +1200

    steps towards full ios cross compiling and linking to godot dependencies.

--- cmd/gd/internal/builder/ios.go
+++ cmd/gd/internal/builder/ios.go
@@ -1,6 +1,7 @@
 package builder
 
 import (
+	"embed"
 	_ "embed"
 	"fmt"
 	"os"
@@ -16,6 +17,13 @@ import (
 var (
 	//go:embed bundled/ios/Info.plist
 	info_plist string
+
+	// ios_sdk is a manually prepared "IOS SDK" designed to support cross-compilation of Go/Godot code to arm64-ios targets using "zig cc".
+	// the SDK was constructed by adding each undefined symbol / missing library observed from compilation/linking errors into .tbd files
+	// placed in the expected location.
+	//
+	//go:embed bundled/ios
+	ios_sdk embed.FS
 )
 
 type IOS struct{}
@@ -25,28 +33,30 @@ func (IOS) Build(args ...string) error {
 	if goarch := os.Getenv("GOARCH"); goarch != "" {
 		GOARCH = goarch
 	}
-	if runtime.GOOS != "darwin" {
-		zig, err := tooling.Zig.Lookup()
-		if err != nil {
-			return xray.New(err)
-		}
-		project.SetupFiles(macos_sdk, "bundled/macos", filepath.Join(project.ReleasesDirectory, "darwin", "sdk"))
-		DARWIN_SDK, err := filepath.Abs(filepath.Join(project.ReleasesDirectory, "darwin", "sdk"))
-		if err != nil {
-			return xray.New(err)
-		}
-		switch GOARCH {
-		case "arm64":
-			if err := os.Setenv("CC", zig+" cc -target aarch64-macos -F "+DARWIN_SDK+"/Frameworks -L"+DARWIN_SDK+"/lib -I"+DARWIN_SDK+"/include"); err != nil {
-				return xray.New(err)
-			}
-			if err := os.Setenv("GOARCH", "arm64"); err != nil {
-				return xray.New(err)
-			}
-		default:
-			return fmt.Errorf("gd build: cannot cross-compile linux %v on %v", GOARCH, runtime.GOOS)
-		}
+	zig, err := tooling.Zig.Lookup()
+	if err != nil {
+		return xray.New(err)
 	}
+	if err := project.SetupFiles(ios_sdk, "bundled/ios", filepath.Join(project.ReleasesDirectory, "ios", "sdk")); err != nil {
+		return xray.New(err)
+	}
+	project.SetupFiles(macos_sdk, "bundled/macos", filepath.Join(project.ReleasesDirectory, "ios", "sdk"))
+	DARWIN_SDK, err := filepath.Abs(filepath.Join(project.ReleasesDirectory, "ios", "sdk"))
+	if err != nil {
+		return xray.New(err)
+	}
+	switch GOARCH {
+	case "arm64":
+		if err := os.Setenv("CC", zig+" cc -target aarch64-ios -F "+DARWIN_SDK+"/Frameworks -L"+DARWIN_SDK+"/lib -I"+DARWIN_SDK+"/include -Wno-nullability-completeness"); err != nil {
+			return xray.New(err)
+		}
+		if err := os.Setenv("GOARCH", "arm64"); err != nil {
+			return xray.New(err)
+		}
+	default:
+		return fmt.Errorf("gd build: cannot cross-compile linux %v on %v", GOARCH, runtime.GOOS)
+	}
+	fmt.Println(args)
 	if err := tooling.Go.Action("build", args, "-buildmode=c-archive", "-o", filepath.Join(project.GraphicsDirectory, fmt.Sprintf("darwin_%v.a", GOARCH))); err != nil {
 		return xray.New(err)
 	}
@@ -65,8 +75,8 @@ func (IOS) Build(args ...string) error {
 	return nil
 }
 
-func (ios IOS) BuildMain(...string) error {
-	if err := ios.Build(); err != nil {
+func (ios IOS) BuildMain(args ...string) error {
+	if err := ios.Build(args...); err != nil {
 		return xray.New(err)
 	}
 	if err := os.Chdir(project.GraphicsDirectory); err != nil {
