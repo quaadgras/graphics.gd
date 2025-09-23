07d71da211e8304a854912c9e42241c60aeb52af
Madeleine Day <madeleine.day@hotmail.com>
2025-09-23 23:39:21 +1200

    images

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ function loadJSON() {
             // populate #examples div with data
             const examplesDiv = document.getElementById("examples");
             data.forEach(example => {
-                const exampleElement = document.createElement("div");
+                const exampleElement = document.createElement("a");
                 exampleElement.textContent = example;
                 examplesDiv.appendChild(exampleElement);
+                exampleElement.href = `${example}`;
+
+                // Add img element
+                const imgElement = document.createElement("img");
+                imgElement.src = `${example}.webp`;
+                exampleElement.appendChild(imgElement);
             });
         })
         .catch(error => console.error("Error loading JSON:", error));
