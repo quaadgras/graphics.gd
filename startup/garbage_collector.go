6aa8d1159775288c4972354519a4353fbb409dc9
Quentin Quaadgras <email@quentinquaadgras.com>
2025-10-07 20:14:57 +1300

    move other startup routines to prefer main loop callbacks instead of defer

--- startup/garbage_collector.go
+++ startup/garbage_collector.go
@@ -1,9 +1,15 @@
 package startup
 
 import (
+	"os"
+	"path/filepath"
+
+	"graphics.gd/classdb/EditorInterface"
+	EngineClass "graphics.gd/classdb/Engine"
 	"graphics.gd/internal/gdextension"
 	"graphics.gd/internal/pointers"
 	"graphics.gd/variant/Callable"
+	"graphics.gd/variant/String"
 
 	_ "unsafe"
 )
@@ -14,6 +20,24 @@ func keep_reachable_instances_alive()
 func init() {
 	gdextension.On.MainLoop.FirstFrame = func() {
 		Callable.Cycle()
+		if EngineClass.IsEditorHint() {
+			settings := EditorInterface.GetEditorSettings()
+			if settings.GetSetting("export/android/java_sdk_path").(String.Readable).String() == "" {
+				GDPATH := os.Getenv("GDPATH")
+				if GDPATH == "" {
+					GDPATH = filepath.Join(os.Getenv("HOME"), "gd")
+				}
+				settings.SetSetting("export/android/java_sdk_path", GDPATH)
+			}
+		}
+		if pause_main != nil {
+			resume_main()
+		} else {
+			if !hasLoaded {
+				close(loaded)
+				hasLoaded = true
+			}
+		}
 	}
 	gdextension.On.MainLoop.EveryFrame = func() {
 		Callable.Cycle()
