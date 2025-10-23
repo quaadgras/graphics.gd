04fc0dd86de168b74cc32d42b7d4a6533bd6b544
Quentin Quaadgras <email@quentinquaadgras.com>
2025-10-23 14:02:21 +1300

    simplified gdreference

--- internal/gdreference/object.go
+++ internal/gdreference/object.go
@@ -2,20 +2,37 @@ package gdreference
 
 import (
 	"runtime"
-	"sync/atomic"
 
 	"graphics.gd/internal/gdextension"
 	"graphics.gd/internal/threadsafe"
 )
 
+// Object reference that's safe to use from a single goroutine.
 type Object struct {
 	_ [0]*Object
+
+	// Cases
+	//
+	// sentinel == nil
+	// 	the object is an unsafe/raw reference
+	//
+	// assigned.objectID == 0 && sentinel.objectID == 0
+	// 	the object is owned by Go and there is a cleanup associated with it.
+	//
+	// assigned.objectID == 0 && sentinel.objectID != 0
+	// 	the values in the sentinel object override the values in the assigned object
+	//
+	// assigned.objectID != 0 && sentinel.objectID == 0
+	// 	the object is owned by the mainthread and has been used within the last frame.
+	//
+	// assigned.objectID != sentinel.objectID
+	// 	the object may have been invalidated, ignore the sentinel and only lookup the assigned objectID
 
 	assigned object
 	sentinel *object
 }
 
-func New(obj gdextension.Object, id gdextension.ObjectID, on_main_thread bool) Object {
+func NewObject(obj gdextension.Object, id gdextension.ObjectID, on_main_thread bool) Object {
 	var sentinel *object
 	if on_main_thread {
 		if pool_free != nil {
@@ -41,69 +58,75 @@ func New(obj gdextension.Object, id gdextension.ObjectID, on_main_thread bool) O
 			cleanups.Remove(id)
 		}, id))
 	}
-	atomic.StoreUint64(&sentinel.objectID, uint64(id))
-	atomic.StoreUint64(&sentinel.inEngine, uint64(obj))
-	atomic.StoreUint64(&sentinel.checksum, uint64(id)^uint64(obj))
+	sentinel.objectID = id
+	sentinel.inEngine = obj
 	return Object{
 		sentinel: sentinel,
 		assigned: object{
-			objectID: uint64(id),
-			inEngine: uint64(obj),
-			checksum: uint64(id) ^ uint64(obj),
+			objectID: id,
+			inEngine: obj,
 		},
 	}
 }
 
-func (obj Object) lookup() (gdextension.Object, bool) {
-	if obj.sentinel == nil {
-		return gdextension.Object(obj.assigned.inEngine), true
-	}
-	objectID := atomic.LoadUint64(&obj.sentinel.objectID)
-	inEngine := atomic.LoadUint64(&obj.sentinel.inEngine)
-	checksum := atomic.LoadUint64(&obj.sentinel.checksum)
-	if checksum != objectID^uint64(inEngine) || objectID != obj.assigned.objectID {
-		raw := gdextension.Host.Objects.Lookup(gdextension.ObjectID(obj.assigned.objectID))
-		return raw, raw != 0
-	}
-	return gdextension.Object(inEngine), true
+func RawObject(obj gdextension.Object, id gdextension.ObjectID) Object {
+	return Object{assigned: object{objectID: id, inEngine: obj}}
 }
 
-func (obj Object) end() (gdextension.Object, bool) {
+func (obj Object) lookup() gdextension.Object {
 	if obj.sentinel == nil {
-		return gdextension.Object(obj.assigned.inEngine), true
+		return obj.assigned.inEngine
 	}
-	objectID := atomic.LoadUint64(&obj.sentinel.objectID)
-	inEngine := atomic.LoadUint64(&obj.sentinel.inEngine)
-	checksum := atomic.LoadUint64(&obj.sentinel.checksum)
-	if checksum != objectID^uint64(inEngine) || objectID != obj.assigned.objectID {
-		return 0, false
+	objectID := obj.sentinel.objectID
+	inEngine := obj.sentinel.inEngine
+	if objectID != obj.assigned.objectID && obj.assigned.objectID != 0 {
+		return gdextension.Host.Objects.Lookup(obj.assigned.objectID)
 	}
-	if atomic.CompareAndSwapUint64(&obj.sentinel.inEngine, inEngine, 0) {
-		atomic.StoreUint64(&obj.sentinel.checksum, 0)
-		atomic.StoreUint64(&obj.sentinel.objectID, 0)
-		if cleanup, ok := cleanups.Lookup(gdextension.ObjectID(objectID)); ok {
+	return inEngine
+}
+
+func (obj Object) end() gdextension.Object {
+	if obj.sentinel == nil {
+		return obj.assigned.inEngine
+	}
+	objectID := obj.sentinel.objectID
+	inEngine := obj.sentinel.inEngine
+	if obj.assigned.objectID == 0 {
+		obj.sentinel.inEngine = 0
+		return inEngine
+	}
+	if objectID != obj.assigned.objectID {
+		return 0
+	}
+	obj.sentinel.inEngine = 0
+	if obj.assigned.objectID == 0 && objectID == 0 {
+		if cleanup, ok := cleanups.Lookup(objectID); ok {
 			cleanup.Stop()
-			cleanups.Remove(gdextension.ObjectID(objectID))
+			cleanups.Remove(objectID)
 			select {
 			case free <- obj.sentinel:
 			default:
 			}
 		}
-		return gdextension.Object(inEngine), true
 	}
-	return 0, false
+	return inEngine
 }
 
 func (obj Object) Free() {
-	if raw, ok := obj.end(); ok {
+	if raw := obj.end(); raw != 0 {
 		gdextension.Host.Objects.Unsafe.Free(raw)
 	}
 }
 
 type object struct {
-	objectID uint64
-	inEngine uint64
-	checksum uint64
+	objectID gdextension.ObjectID
+	inEngine gdextension.Object
+	lifetime *lifetime
+}
+
+type lifetime struct {
+	cleanup runtime.Cleanup
+	created uint64
 }
 
 var tail int
