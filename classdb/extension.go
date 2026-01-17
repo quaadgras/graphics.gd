c2d0135fd5c03c8dd1c90c9cd75779210b873334
Quentin Quaadgras <email@quentinquaadgras.com>
2026-01-17 13:06:53 +1300

    improve wording of ExtensionInherits

--- classdb/extension.go
+++ classdb/extension.go
@@ -11,9 +11,9 @@ type ExtensionTo[T gd.IsClass] interface {
 	super() T
 }
 
-// ExtensionInherits can be used to enable a Go class to inherit another, this means
-// they will show up nested in the Node selection menu within the editor. This is an
-// advanced technique and any desired helper methods like AsNode, AsParent are not
+// ExtensionInherits can be embedded to have a Go extension class inherit another, this
+// means they will show up nested in the Node selection menu within the editor. This is
+// an advanced technique and any desired helper methods like AsNode, AsParent are not
 // included and will need to be defined as methods on T.
 //
 // The super class S can be accessed by calling Super().
